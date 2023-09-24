312a295c7815a9c60d662abab45d5d74eb6fe65a
AdarsH <adarshrawat8304@gmail.com>
2023-09-24 11:47:38 +0000

    :build

--- pages/components/Herobar.js
+++ pages/components/Herobar.js
@@ -1,87 +1,92 @@
-import React from 'react'
-import Particles from "react-tsparticles";
+import React, { useCallback } from 'react'
+import Particles, { ISourceOptions } from "react-tsparticles";
+import { loadSlim } from "tsparticles-slim";
 import Link from 'next/link';
 
 const Herobar = () => {
+  const particlesInit = useCallback(async engine => {
+    console.log(engine);
+    await loadSlim(engine);
+  }, []);
+
+  const particlesLoaded = useCallback(async container => {
+    await console.log(container);
+  }, []);
+
   return (
     <div className='h-11/12 relative'>
       <Particles
         id="tsparticles"
+        init={particlesInit}
+        loaded={particlesLoaded}
         options={{
           background: {
             color: {
-              value: "#FFFFFF",
+              value: "#ffffff",
             },
           },
           fpsLimit: 60,
           interactivity: {
-            events: {
-              onClick: {
-                mode: "push",
-              },
-              onHover: {
-                enable: true,
-                mode: "repulse",
-              },
-              resize: true,
-            },
-            modes: {
-              bubble: {
-                distance: 400,
-                duration: 2,
-                opacity: 0.8,
-                size: 40,
-              },
-              push: {
-                quantity: 4,
-              },
-              repulse: {
-                distance: 100,
-                duration: 0.4,
-              },
-            },
-          },
-          particles: {
-            color: {
-              value: "#000000",
-            },
-            links: {
-              color: "#000000",
-              distance: 150,
+          events: {
+            onClick: {
               enable: true,
-              opacity: 1,
-              width: 2,
+              mode: "push",
             },
-            collisions: {
+            onHover: {
               enable: true,
+              mode: "repulse",
             },
-            move: {
-              direction: "none",
-              enable: true,
-              outMode: "bounce",
-              random: false,
-              speed: 0.5,
-              straight: false,
-            },
-            number: {
-              density: {
-                enable: true,
-                area: 800,
-              },
-              value: 40,
-            },
-            opacity: {
-              value: 0.8,
-            },
-            shape: {
-              type: "circle",
-            },
-            size: {
-              random: true,
-              value: 4,
-            },
-          },
-          detectRetina: true,
+                        resize: true,
+                    },
+                    modes: {
+                        push: {
+                            quantity: 4,
+                        },
+                        repulse: {
+                            distance: 200,
+                            duration: 0.4,
+                        },
+                    },
+                },
+                particles: {
+                    color: {
+                        value: "#b5b5b5",
+                    },
+                    links: {
+                        color: "#b5b5b5",
+                        distance: 150,
+                        enable: true,
+                        opacity: 0.7,
+                        width: 1,
+                    },
+                    move: {
+                        direction: "none",
+                        enable: true,
+                        outModes: {
+                            default: "bounce",
+                        },
+                        random: false,
+                        speed: 2,
+                        straight: false,
+                    },
+                    number: {
+                        density: {
+                            enable: true,
+                            area: 800,
+                        },
+                        value: 70,
+                    },
+                    opacity: {
+                        value: 0.5,
+                    },
+                    shape: {
+                        type: "circle",
+                    },
+                    size: {
+                        value: { min: 1, max: 5 },
+                    },
+                },
+                detectRetina: true,
         }}
       />
 
