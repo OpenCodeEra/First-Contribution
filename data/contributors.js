ab20e99d751ddc7bf53eb1e03705eaff0126b955
Adarsh Rawat <adarshbunny293@gmail.com>
2023-09-24 19:06:26 +0530

    Updated contributors.js

--- data/contributors.js
+++ data/contributors.js
@@ -7,6 +7,13 @@ const contributors = [
     github: "https://github.com/AKACHI-4",
     gender: "male"
   },
+  {
+    name: "Adarsh Rawat", 
+    skill: "Web Dev Android ML UI/UX  ", 
+    linkedin: "https://www.linkedin.com/in/adarsh-rawat/", 
+    github: "https://github.com/AdarshRawat1",
+    gender: "male"
+  },
 ]
 
 export default contributors; 
