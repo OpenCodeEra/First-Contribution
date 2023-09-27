10051ebba8e5e2c823d5f268f46244801833bbcb
Vijay Gunwant <vijaygunwant21@gmail.com>
2023-09-27 08:41:45 +0530

    Fixed class= to ClassName = in Navbar and Footer

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -2,27 +2,27 @@ import React from 'react'
 
 export const Footer = (props) => {
   return (
-        <footer class="opacity-90 relative bg-gray-200 rounded-t-lg shadow">
-            <div class="w-full max-w-screen-xl mx-auto p-4 md:py-6">
-                <div class="sm:flex sm:items-center sm:justify-between">
-                    <a href="#" class="flex items-center mb-4 sm:mb-0">
+        <footer className="opacity-90 relative bg-gray-200 rounded-t-lg shadow">
+            <div className="w-full max-w-screen-xl mx-auto p-4 md:py-6">
+                <div className="sm:flex sm:items-center sm:justify-between">
+                    <a href="#" className="flex items-center mb-4 sm:mb-0">
                         
-                        <span class="self-center text-2xl font-semibold whitespace-nowrap text-black">Open Code Era</span>
+                        <span className="self-center text-2xl font-semibold whitespace-nowrap text-black">Open Code Era</span>
                     </a>
-                    <ul class="flex flex-wrap items-center gap-6 mb-6 text-lg text-gray-800 sm:mb-0">
+                    <ul className="flex flex-wrap items-center gap-6 mb-6 text-lg text-gray-800 sm:mb-0">
                         <li>
-                            <a href="#" class="hover:underline hover:text-blue-800">About</a>
+                            <a href="#" className="hover:underline hover:text-blue-800">About</a>
                         </li>
                         <li>
-                            <a href="#" class="hover:underline hover:text-blue-800">Contact</a>
+                            <a href="#" className="hover:underline hover:text-blue-800">Contact</a>
                         </li>
                         <li>
-                            <a href="#" class="hover:underline hover:text-blue-800">Join us</a>
+                            <a href="#" className="hover:underline hover:text-blue-800">Join us</a>
                         </li>
                     </ul>
                 </div>
-                <hr class="my-4 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-6" />
-                <span class="block text-sm sm:text-center text-gray-800">© 2023 <a href="#" class="hover:underline text-semibold hover:text-blue-700">GDSC GEU</a>. All Rights Reserved.</span>
+                <hr className="my-4 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-6" />
+                <span className="block text-sm sm:text-center text-gray-800">© 2023 <a href="#" className="hover:underline text-semibold hover:text-blue-700">GDSC GEU</a>. All Rights Reserved.</span>
             </div>
         </footer>
   )
--- pages/components/Navbar.js
+++ pages/components/Navbar.js
@@ -4,33 +4,33 @@ import Link from "next/link";
 const Navbar = (props) => { 
 
     return (
-        <nav class="bg-opacity-80n flex justify-between bg-gray-200 text-gray-800 w-full z-50 sticky top-0 left-0">
-            <div class="px-5 xl:px-12 py-6 flex w-full items-center justify-center">
-                <a class="text-2xl font-semibold text-gray-800" href="#">
+        <nav className="bg-opacity-80n flex justify-between bg-gray-200 text-gray-800 w-full z-50 sticky top-0 left-0">
+            <div className="px-5 xl:px-12 py-6 flex w-full items-center justify-center">
+                <a className="text-2xl font-semibold text-gray-800" href="#">
                 Open code Era
                 </a>
-                <ul class="hidden md:flex justify-around px-2 mx-auto text-lg font-heading space-x-16">
-                <li><Link class="hover:text-blue-800 hover:underline" href="#">Home</Link></li>
-                <li><Link class="hover:text-blue-800 hover:underline" href="#contributors">Contributors</Link></li>
-                <li><Link class="hover:text-blue-800 hover:underline" href="#">About us</Link></li>
+                <ul className="hidden md:flex justify-around px-2 mx-auto text-lg font-heading space-x-16">
+                <li><Link className="hover:text-blue-800 hover:underline" href="#">Home</Link></li>
+                <li><Link className="hover:text-blue-800 hover:underline" href="#contributors">Contributors</Link></li>
+                <li><Link className="hover:text-blue-800 hover:underline" href="#">About us</Link></li>
                 </ul>
                 <Link href={'https://github.com/OpenCodeEra/first-contribution'} className="flex items-center space-x-5">
-                {/* <a class="hover:text-gray-200" href="#">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                {/* <a className="hover:text-gray-200" href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z" />
                     </svg>
                 </a> */}
-                {/* <a class="flex items-center hover:text-gray-200" href="#">
-                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                {/* <a className="flex items-center hover:text-gray-200" href="#">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
                     </svg>
-                    <span class="flex absolute -mt-5 ml-4">
-                    <span class="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75"></span>
-                        <span class="relative inline-flex rounded-full h-3 w-3 bg-pink-500">
+                    <span className="flex absolute -mt-5 ml-4">
+                    <span className="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75"></span>
+                        <span className="relative inline-flex rounded-full h-3 w-3 bg-pink-500">
                         </span>
                     </span>
                 </a> */}
-                    <button type="button" class="text-white bg-gray-500 border-1 border-dashed border-white hover:bg-blue-600 font-medium rounded-lg text-sm px-3 py-1.5 transition duration-300 ease-in-out mr-2">Contribute now</button>                
+                    <button type="button" className="text-white bg-gray-500 border-1 border-dashed border-white hover:bg-blue-600 font-medium rounded-lg text-sm px-3 py-1.5 transition duration-300 ease-in-out mr-2">Contribute now</button>                
                 </Link>
             </div>
         </nav>
